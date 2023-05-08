3189caaa5f91b7af80052024deed29475a7ceeca
EstebanS3 <esteban.sanchez.sanchez@est.una.ac.cr>
2023-05-07 20:06:24 -0600

    Primera Version de Ejemplo de Almacenamiento en SQLITE Android Java

--- app/src/main/java/com/example/almacenamientosqlite/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/almacenamientosqlite/AdminSQLiteOpenHelper.java
@@ -11,7 +11,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
     public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
-
+//Creacion de la tabla
     @Override
     public void onCreate(SQLiteDatabase BaseDeDatos) {
         BaseDeDatos.execSQL("create table articulos(codigo int primary key, descripcion text, precio real)");
